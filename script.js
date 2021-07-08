93982259ce84d2682e6538f9e642ba3afccc2f0d
Écio Ferraz <ecioferraz@hotmail.com>
2021-07-08 16:38:32 -0300

    requisitos 4 e 5

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
+window.onload = function() {
+
 let color1 = document.querySelector('#color-palette').firstElementChild;
 let color2 = document.querySelector('#color-palette').children[1];
 let color3 = document.querySelector('#color-palette').children[2];
 let color4 = document.querySelector('#color-palette').lastElementChild;
 
+
 color1.style.backgroundColor = 'black';
 color2.style.backgroundColor = 'blue';
 color3.style.backgroundColor = 'yellow';
 color4.style.backgroundColor = 'purple';
+
+
+}
