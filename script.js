ad63541e9b39fd0af75d951dd4c812c17f0f2cec
Écio Ferraz <ecioferraz@hotmail.com>
2021-07-08 19:21:55 -0300

    requisito 8

--- script.js
+++ script.js
@@ -1,27 +1,36 @@
 window.onload = function() {
 
-let color1 = document.querySelector('#color-palette').firstElementChild;
-let color2 = document.querySelector('#color-palette').children[1];
-let color3 = document.querySelector('#color-palette').children[2];
-let color4 = document.querySelector('#color-palette').lastElementChild;
+  let color1 = document.querySelector('#color-palette').firstElementChild;
+  let color2 = document.querySelector('#color-palette').children[1];
+  let color3 = document.querySelector('#color-palette').children[2];
+  let color4 = document.querySelector('#color-palette').lastElementChild;
 
 
-color1.style.backgroundColor = 'black';
-color2.style.backgroundColor = 'blue';
-color3.style.backgroundColor = 'yellow';
-color4.style.backgroundColor = 'purple';
+  color1.style.backgroundColor = 'black';
+  color2.style.backgroundColor = 'blue';
+  color3.style.backgroundColor = 'yellow';
+  color4.style.backgroundColor = 'purple';
 
-color1.classList.add('selected');
+  color1.classList.add('selected');
 
-function addSelected(event) {
-  let selectedColor = document.querySelector('.selected');
-  selectedColor.classList.remove('selected');
-  event.target.classList.add('selected');
-}
+  let color = color1.style.backgroundColor;
 
-color1.addEventListener('click', addSelected);
-color2.addEventListener('click', addSelected);
-color3.addEventListener('click', addSelected);
-color4.addEventListener('click', addSelected);
+  function addSelected(event) {
+    let selectedColor = document.querySelector('.selected');
+    selectedColor.classList.remove('selected');
+    event.target.classList.add('selected');
+    color = event.target.style.backgroundColor;
+  }
 
+  color1.addEventListener('click', addSelected);
+  color2.addEventListener('click', addSelected);
+  color3.addEventListener('click', addSelected);
+  color4.addEventListener('click', addSelected);
+
+  let pixel = document.querySelector('#pixel-board');
+
+  pixel.addEventListener('click', function (event) {
+  event.target.style.backgroundColor = color;
+    });
+  
 }
