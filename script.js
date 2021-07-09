e9af37f838555035b60238234af2ec76a2e66860
Écio Ferraz <ecioferraz@hotmail.com>
2021-07-09 13:33:28 -0300

    refatorando

--- script.js
+++ script.js
@@ -1,18 +1,63 @@
 window.onload = function() {
+  // 1. Cria o título com id 'title'
+  let h1 = document.createElement('h1');
+  h1.innerHTML = 'Paleta de Cores';
+  h1.id = 'title';
+  document.body.prepend(h1);
+
+  // 2 e 3. Adiciona paleta de quatro cores
+  let palette = document.createElement('div');
+  palette.id = 'color-palette';
+  document.body.insertBefore(palette, h1.nextElementSibling);
+
+  let colorPalette = document.querySelector('#color-palette');
+
+  for (let index = 0; index < 4; index += 1) {
+    let div = document.createElement('div');
+    div.className = 'color';
+
+    colorPalette.appendChild(div);
+  }
 
   let color1 = document.querySelector('#color-palette').firstElementChild;
   let color2 = document.querySelector('#color-palette').children[1];
   let color3 = document.querySelector('#color-palette').children[2];
   let color4 = document.querySelector('#color-palette').lastElementChild;
 
-
   color1.style.backgroundColor = 'black';
   color2.style.backgroundColor = 'blue';
   color3.style.backgroundColor = 'yellow';
   color4.style.backgroundColor = 'purple';
 
+  // 4 e 5. Adiciona um quadro de pixels
+  let pixelBoard = document.createElement('div');
+  pixelBoard.id = 'pixel-board';
+  document.body.insertBefore(pixelBoard, palette.nextElementSibling);
+
+  let pixels = document.querySelector('#pixel-board');
+  let n = 5;
+
+  for (let index = 0; index < n; index += 1) {
+    let div = document.createElement('div');
+    pixels.appendChild(div);
+    }
+
+    let pixelsChildren = pixels.children;
+    console.log(pixelsChildren)
+
+  for (let index = 0; index < n; index += 1) {
+    for (let index = 0; index < n; index += 1) {
+    let pixelDiv = document.createElement('div');
+    pixelDiv.className = 'pixel';
+
+    pixelsChildren[index].appendChild(pixelDiv);
+    }
+  }
+
+  // 6. Cor preta inicial
   color1.classList.add('selected');
 
+  // 7. Altera cor selecionada
   let color = color1.style.backgroundColor;
 
   function addSelected(event) {
@@ -27,19 +72,25 @@ window.onload = function() {
   color3.addEventListener('click', addSelected);
   color4.addEventListener('click', addSelected);
 
+  // 8. Pinta os pixels com a cor selecionada
   let pixel = document.querySelector('#pixel-board');
 
   pixel.addEventListener('click', function(event) {
   event.target.style.backgroundColor = color;
   });
-  
+
+  // 9. Cria o botão para limpar os pixels
+  let button = document.createElement('button');
+  button.innerHTML = 'Limpar'
+  button.id = 'clear-board';
+  document.body.insertBefore(button, colorPalette.nextElementSibling);
+
   let btn = document.querySelector('#clear-board');
-  let pixels = document.getElementsByClassName('pixel');
+  let paintPixels = document.getElementsByClassName('pixel');
 
   btn.addEventListener('click', function() {
-    for (index = 0; index < pixels.length; index += 1) {
-      pixels[index].style.backgroundColor = 'white';
+    for (index = 0; index < paintPixels.length; index += 1) {
+      paintPixels[index].style.backgroundColor = 'white';
     }
   })
-
 }
